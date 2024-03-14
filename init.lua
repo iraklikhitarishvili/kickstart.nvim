b0afdccd9780ab53437377303901a3b2a46c8725
Irakli Khitarishvili <7589748+iraklikhitarishvili@users.noreply.github.com>
2024-03-14 15:02:22 +0400

    style: update color scheme

--- init.lua
+++ init.lua
@@ -89,7 +89,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim',       opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
@@ -195,6 +195,19 @@ require('lazy').setup({
     name = "catppuccin",
     priority = 1000,
     config = function()
+      require("catppuccin").setup(
+        {
+          flavour = "mocha",
+          term_colors = true,
+          color_overrides = {
+            mocha = {
+              base = "#1a1a1a",
+              mantle = "#1a1a1a",
+              crust = "#1f1f1f",
+            },
+          },
+        }
+      )
       vim.cmd.colorscheme 'catppuccin'
     end
   },
